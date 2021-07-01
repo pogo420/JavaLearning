d2186049468e0df07d60a6736149ed68559c4a63
Arnab Mukherjee <am0072008@gmail.com>
2021-07-01 15:21:50 +0530

    retry and repeat

--- src/com/udemy_dilip/reactive/reactive-programming-using-reactor-main/src/main/java/com/learnreactiveprogramming/service/MovieReactiveService.java
+++ src/com/udemy_dilip/reactive/reactive-programming-using-reactor-main/src/main/java/com/learnreactiveprogramming/service/MovieReactiveService.java
@@ -7,7 +7,10 @@ import com.learnreactiveprogramming.exception.MovieException;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+import reactor.util.retry.RetryBackoffSpec;
 
+import java.time.Duration;
 import java.util.List;
 
 @Slf4j
@@ -21,7 +24,7 @@ public class MovieReactiveService {
         this.reviewService = reviewService;
     }
 
-    public Flux<Movie> getAllMovies(){
+    public Flux<Movie> getAllMovies() {
         Flux<MovieInfo> movieInfoFlux = movieInfoService.retrieveMoviesFlux();
 
         return movieInfoFlux.flatMap(movieInfo -> {
@@ -34,7 +37,40 @@ public class MovieReactiveService {
         );
     }
 
-    public Mono<Movie> getMovieById(long movieId){
+
+    public Flux<Movie> getAllMovies_retry() {
+        Flux<MovieInfo> movieInfoFlux = movieInfoService.retrieveMoviesFlux();
+
+        return movieInfoFlux.flatMap(movieInfo -> {
+            Mono<List<Review>> listMono = reviewService.retrieveReviewsFlux(movieInfo.getMovieInfoId()).collectList();
+            return listMono.map( reviews -> new Movie(movieInfo, reviews));
+        }).onErrorMap(ex->{
+                    log.error("Exception is", ex);
+                    throw new MovieException(ex.getMessage());
+                }
+        ).retry(3);
+    }
+
+
+    public Flux<Movie> getAllMovies_retryWhen() {
+
+        RetryBackoffSpec retrySpec = Retry.backoff(3, Duration.ofMillis(500)).filter(ex -> {
+            return ex instanceof MovieException;
+        });
+
+        Flux<MovieInfo> movieInfoFlux = movieInfoService.retrieveMoviesFlux();
+
+        return movieInfoFlux.flatMap(movieInfo -> {
+            Mono<List<Review>> listMono = reviewService.retrieveReviewsFlux(movieInfo.getMovieInfoId()).collectList();
+            return listMono.map( reviews -> new Movie(movieInfo, reviews));
+        }).onErrorMap(ex->{
+                    log.error("Exception is", ex);
+                    throw new MovieException(ex.getMessage());
+                }
+        ).retryWhen(retrySpec);
+    }
+
+    public Mono<Movie> getMovieById(long movieId) {
         Mono<MovieInfo> movieInfoMono = movieInfoService.retrieveMovieInfoMonoUsingId(movieId);
         Mono<List<Review>> reviewFlux = reviewService.retrieveReviewsFlux(movieId).collectList();
 
--- src/com/udemy_dilip/reactive/reactive-programming-using-reactor-main/src/test/java/com/learnreactiveprogramming/service/MovieReactiveServiceMockTest.java
+++ src/com/udemy_dilip/reactive/reactive-programming-using-reactor-main/src/test/java/com/learnreactiveprogramming/service/MovieReactiveServiceMockTest.java
@@ -1,13 +1,20 @@
 package com.learnreactiveprogramming.service;
 
+import com.learnreactiveprogramming.domain.Movie;
+import com.learnreactiveprogramming.exception.MovieException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class MovieReactiveServiceMockTest {
@@ -23,7 +30,73 @@ class MovieReactiveServiceMockTest {
 
     @Test
     void getAllMovies() {
-        
-        movieReactiveService.getAllMovies();
+
+        Mockito.when(movieInfoService.retrieveMoviesFlux())
+                .thenCallRealMethod();  // method stubs
+
+        Mockito.when(reviewService.retrieveReviewsFlux(anyLong()))
+                .thenCallRealMethod();
+
+        Flux<Movie> allMovies = movieReactiveService.getAllMovies();
+
+        StepVerifier.create(allMovies).expectNextCount(3).verifyComplete();
     }
+
+    @Test
+    void getAllMovies_1() {
+
+        Mockito.when(movieInfoService.retrieveMoviesFlux())
+                .thenCallRealMethod();  // method stubs
+
+        Mockito.when(reviewService.retrieveReviewsFlux(anyLong()))
+                .thenThrow(new MovieException("Ola"));
+
+        Flux<Movie> allMovies = movieReactiveService.getAllMovies();
+
+        StepVerifier
+                .create(allMovies)
+                .expectError(MovieException.class)
+                .verify();
+    }
+
+    @Test
+    void getAllMovies_retry() {
+
+        Mockito.when(movieInfoService.retrieveMoviesFlux())
+                .thenCallRealMethod();  // method stubs
+
+        Mockito.when(reviewService.retrieveReviewsFlux(anyLong()))
+                .thenThrow(new RuntimeException("Ola"));
+
+        Flux<Movie> allMovies = movieReactiveService.getAllMovies_retry();
+
+        StepVerifier
+                .create(allMovies)
+                .expectError(RuntimeException.class)
+                .verify();
+
+        verify(reviewService, times(4))
+                .retrieveReviewsFlux(anyLong());  // checking for 3 times retry
+    }
+
+    @Test
+    void getAllMovies_retryWhen() {
+
+        Mockito.when(movieInfoService.retrieveMoviesFlux())
+                .thenCallRealMethod();  // method stubs
+
+        Mockito.when(reviewService.retrieveReviewsFlux(anyLong()))
+                .thenThrow(new RuntimeException("Ola"));
+
+        Flux<Movie> allMovies = movieReactiveService.getAllMovies_retryWhen();
+
+        StepVerifier
+                .create(allMovies)
+                .expectError(RuntimeException.class)
+                .verify();
+
+        verify(reviewService, times(4))
+                .retrieveReviewsFlux(anyLong());  // checking for 3 times retry
+    }
+
 }
