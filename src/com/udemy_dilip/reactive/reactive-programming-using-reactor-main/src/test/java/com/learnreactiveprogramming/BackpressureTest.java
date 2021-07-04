7c3df0e42e0e13ce6b8817272afa947bb67636a9
Arnab Mukherjee <am0072008@gmail.com>
2021-07-04 14:08:02 +0530

    back-pressure

--- src/com/udemy_dilip/reactive/reactive-programming-using-reactor-main/src/test/java/com/learnreactiveprogramming/BackpressureTest.java
+++ src/com/udemy_dilip/reactive/reactive-programming-using-reactor-main/src/test/java/com/learnreactiveprogramming/BackpressureTest.java
@@ -1,4 +1,48 @@
 package com.learnreactiveprogramming;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+
+@Slf4j
 public class BackpressureTest {
+
+    @Test
+    void testBackPressure(){
+        Flux
+                .range(0, 1000)
+//                .subscribe(num -> log.info("values is: {}", num))
+                .subscribe(new BaseSubscriber<Integer>() {
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+//                        super.hookOnSubscribe(subscription);
+                        subscription.request(10);
+                    }
+
+                    @Override
+                    protected void hookOnNext(Integer value) {
+//                        super.hookOnNext(value);
+                        log.info("values is: {}", value);
+
+                    }
+
+                    @Override
+                    protected void hookOnComplete() {
+//                        super.hookOnComplete();
+                    }
+
+                    @Override
+                    protected void hookOnError(Throwable throwable) {
+//                        super.hookOnError(throwable);
+                    }
+
+                    @Override
+                    protected void hookOnCancel() {
+//                        super.hookOnCancel();
+                    }
+                })
+        ;
+    }
 }
